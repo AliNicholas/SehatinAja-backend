12409ebe3f6b590800a312904c96551121136377
Nicholas Ali <nicholasali.888@gmail.com>
2023-06-07 14:52:00 +0700

    fix secret leak

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,20 +1,13 @@
 const { initializeApp } = require("firebase/app");
 const admin = require("firebase-admin");
-
 const serviceAccount = require("../../credentials/Service_account_key.json");
+const config = require("../../credentials/firebaseConfig.json");
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
-const firebaseConfig = {
-  apiKey: "AIzaSyC4NpJCCNXt3ClCDvl10tD12dsfYMet44k",
-  authDomain: "sehatinaja-c7205.firebaseapp.com",
-  projectId: "sehatinaja-c7205",
-  storageBucket: "sehatinaja-c7205.appspot.com",
-  messagingSenderId: "738207239746",
-  appId: "1:738207239746:web:36e0e915be0cb1f55367db",
-  measurementId: "G-CWVTKL4EQ3",
-};
+const firebaseConfig = config;
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
