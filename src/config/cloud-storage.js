ac0098e5263e53a7d7140454c820c56253e4819a
Nicholas Ali <nicholasali.888@gmail.com>
2023-06-13 16:22:12 +0700

    f

---
+++ src/config/cloud-storage.js
@@ -0,0 +1,16 @@
+const { Storage } = require("@google-cloud/storage");
+
+// credentials-key
+const storage = new Storage({
+  projectId: process.env.CS_PROJECT_ID,
+  keyFilename: "credentials/cloud-storage.json",
+});
+
+const bucketName = process.env.CS_BUCKET_NAME;
+
+const bucket = storage.bucket(bucketName); // my bucket name in the cloud
+
+module.exports = {
+  bucket,
+  bucketName,
+};
